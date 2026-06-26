008696de91ab0c1ae92cc1ef3e262c15c7f7f7c4
James Bannon <j.bannon90@gmail.com>
2026-06-26 13:49:27 -0400

    initial push

--- workflow/scripts/analyze_embeddings.py
+++ workflow/scripts/analyze_embeddings.py
@@ -1,3 +1,4 @@
+from textwrap import wrap
 from damply import dirs
 import pandas as pd
 import sys
@@ -32,29 +33,43 @@ for exp in ['Structure','Response']:
     emb_df = emb_df[~(emb_df['MOA'].isin(['Receptor Targeted','Miscellaneous',
                                          'Enzymes','Ion Channels','Ion Channels & Transporters']))]
 
-
+    
+    keep_moas = pd.DataFrame(emb_df['Mechanism.of.Action'].value_counts()).reset_index()
+    keep_moas = keep_moas['Mechanism.of.Action'].values[:5]
+    # print(keep_moas)
+    # sys.exit()
     embeddings = emb_df[[c for c in emb_df.columns if c not in ['HDD.Compound.ID','Mechanism.of.Action','MOA']]].values
     U = fitter.fit_transform(embeddings)
-# U = sclr.fit_transform(U)
+    # U = sclr.fit_transform(U)
 
 
     plot_df = pd.DataFrame({'UMAP-1':U[:,0], 
                             'UMAP-2':U[:,1],
-                            'MOA':emb_df['MOA'].values})
+                            'MOA':emb_df['Mechanism.of.Action'].values})
 # plot_df = plot_df[~(plot_df['MOA'].isin(['Receptor Targeted','Miscellaneous',
     #                                          'Enzymes','Ion Channels','Ion Channels & Transporters']))]
+    plot_df = plot_df[plot_df['MOA'].isin(keep_moas)]
     ax = sns.scatterplot(plot_df,x='UMAP-1',y='UMAP-2',hue='MOA',legend=True)
+    handles, labels = ax.get_legend_handles_labels()
+    labels = [ '\n'.join(wrap(l, 20)) for l in labels]
+    plt.legend(labels = labels)
+    # print(labels)
+    # ax.legend()
+    
     sns.move_legend(ax,"upper left", bbox_to_anchor=(1, 1))
-    plt.tight_layout()
+    
+    
     # plt.scatter(U[:,0],U[:,1], c = requested_compounds['Mechanism.of.Action'].values)
+    plt.title(f"{exp} UMAP")
+    plt.tight_layout()
     plt.savefig(dirs.RESULTS / exp /  f"{exp}_umap.png")
     plt.close()
-
+    
     adjacency = pd.read_csv(dirs.RESULTS / exp / "Adjacency.csv",index_col=0)
     
-    G = nx.from_numpy_array(adjacency.values)
-    nx.draw(G)
-    plt.savefig(dirs.RESULTS / exp / "network.png")
-    plt.close()
+    # G = nx.from_numpy_array(adjacency.values)
+    # nx.draw(G)
+    # plt.savefig(dirs.RESULTS / exp / "network.png")
+    # plt.close()
     
 
